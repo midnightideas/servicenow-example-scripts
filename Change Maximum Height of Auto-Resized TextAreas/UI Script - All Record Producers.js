4e8d872fc2da19722f43f258fbd1cc3b7634220e
Deryck Lio <d@derycklio.com>
2015-09-13 22:29:20 +1000

    Update scripts with more comments

--- Change Maximum Height of Auto-Resized TextAreas/UI Script - All Record Producers.js
+++ Change Maximum Height of Auto-Resized TextAreas/UI Script - All Record Producers.js
@@ -8,6 +8,7 @@
  * Active: True
  */
 document.observe('dom:loaded', function() {
+	// Only include the extra stylesheet if we are in the Record Producer screen
 	if ((typeof g_form != 'undefined') && ('GlideForm' == g_form.type)) {
 		document.styleSheets[0].insertRule("HTML[data-doctype=true] textarea { max-height: 100px !important; }", 0);
 	}
